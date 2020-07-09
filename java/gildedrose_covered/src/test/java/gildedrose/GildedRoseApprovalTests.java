27b87ce5225cec922726a140dd9a9f2e7caa18dd
Paul Moore <pdmoore@gmail.com>
2020-07-09 11:30:32 -0700

    java combo approvals in place

--- java/gildedrose_covered/src/test/java/gildedrose/GildedRoseApprovalTests.java
+++ java/gildedrose_covered/src/test/java/gildedrose/GildedRoseApprovalTests.java
@@ -1,6 +1,7 @@
 package gildedrose;
 
 import org.approvaltests.Approvals;
+import org.approvaltests.combinations.CombinationApprovals;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -14,5 +15,22 @@ public class GildedRoseApprovalTests {
         sut.updateQuality();
         assertEquals("normal, -1, 0", sut.items[0].toString());
         Approvals.verify(sut.items[0].toString());
+    }
+
+    @Test
+    public void test_UpdateQuality_SingleItem() {
+        CombinationApprovals.verifyAllCombinations(
+                this::doUpdateQuality,
+                new String[] {"foo", "Aged Brie", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros"},
+                new Integer[] {-1, 0, 5, 10, 11},    // SellIn
+                new Integer[] { 0, 1, 49, 50 }       // Quality
+        );
+    }
+
+    private String doUpdateQuality(String name, int sellIn, int quality) {
+        Item[] items = new Item[]{new Item(name, sellIn, quality)};
+        GildedRose sut = new GildedRose(items);
+        sut.updateQuality();
+        return sut.items[0].toString();
     }
 }
