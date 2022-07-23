53982434a22b53e40d40191ab8dcfb3bb6b318dc
Paul Moore <pdmoore@gmail.com>
2022-07-22 17:37:16 -0700

    adding tests to fully cover updateQuality

--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -40,6 +40,48 @@ describe("Gilded Rose", function() {
     expect(items[0].sellIn).toBe(7);
   });
 
+  it.each([
+    ["generic item"],
+    ["Aged Brie"],
+    ["Backstage passes to a TAFKAL80ETC concert"],
+  ])('%p item  SellIn will be negative after sellIn date reached', (itemName) => {
+    const sut = new Shop([new Item(itemName, 0, 25)]);
 
+    const items = sut.updateQuality();
+
+    expect(items[0].sellIn).toBe(-1);
+  });
+
+  it("Generic item quality decreases by 1 before sellIn date reached", () => {
+    const sut = new Shop([new Item("generic item", 5, 10)]);
+
+    const items = sut.updateQuality();
+
+    expect(items[0].quality).toBe(9);
+  });
+
+  it("Generic item quality decreases twice as fast after sellIn date reached", () => {
+    const sut = new Shop([new Item("generic item", 0, 10)]);
+
+    const items = sut.updateQuality();
+
+    expect(items[0].quality).toBe(8);
+  });
+
+  it("Generic item quality never decreases below 0", () => {
+    const sut = new Shop([new Item("generic item", 5, 0)]);
+
+    const items = sut.updateQuality();
+
+    expect(items[0].quality).toBe(0);
+  });
+
+  it("Aged Brie quality improves with age", () => {
+    const sut = new Shop([new Item("Aged Brie", 5, 30)]);
+
+    const items = sut.updateQuality();
+
+    expect(items[0].quality).toBe(31);
+  });
 
 });
