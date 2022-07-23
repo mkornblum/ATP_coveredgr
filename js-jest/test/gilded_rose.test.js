c8ac6caea99116d7af4a7c91fd6da123807d03c2
Paul Moore <pdmoore@gmail.com>
2022-07-22 17:31:17 -0700

    adding tests to fully cover updateQuality

--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -1,14 +1,45 @@
 const {Shop, Item} = require("../src/gilded_rose");
 
 describe("Gilded Rose", function() {
-  it("should DESCRIBE WHAT BEHAVIOR IS BEING CONFIRMED", () => {
+  it("Legendary Item quality does not decrease", () => {
     // Arrange
-    const sut = new Shop([new Item("foo", 0, 0)]);
+    const sut = new Shop([new Item("Sulfuras, Hand of Ragnaros", 20, 80)]);
 
     // Act
     const items = sut.updateQuality();
 
     // Assert
-    expect(items[0].name).toBe("fixme");
+    expect(items[0].quality).toBe(80);
   });
+
+  it("Legendary Item never has to be sold", () => {
+    const sut = new Shop([new Item("Sulfuras, Hand of Ragnaros", 1, 80)]);
+
+    const items = sut.updateQuality();
+
+    expect(items[0].sellIn).toBe(1);
+  });
+
+  it("Legendary Item quality does not change after sellin", () => {
+    const sut = new Shop([new Item("Sulfuras, Hand of Ragnaros", -1, 80)]);
+
+    const items = sut.updateQuality();
+
+    expect(items[0].quality).toBe(80);
+  });
+
+  it.each([
+    ["generic item"],
+    ["Aged Brie"],
+    ["Backstage passes to a TAFKAL80ETC concert"],
+  ])('%p item SellIn decreases each update', (itemName) => {
+    const sut = new Shop([new Item(itemName, 8, 10)]);
+
+    const items = sut.updateQuality();
+
+    expect(items[0].sellIn).toBe(7);
+  });
+
+
+
 });
