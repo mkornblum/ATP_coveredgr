9ccdab7b7a011ac4f304721be2902b818a48f6d9
Mark Kornblum <654334+mkornblum@users.noreply.github.com>
2023-03-07 16:27:29 -0800

    Remove snake case from spec docstrings

--- ruby/gilded_rose_spec.rb
+++ ruby/gilded_rose_spec.rb
@@ -27,118 +27,120 @@ describe GildedRose do
       expect(items[0].sell_in).to eq(7)
     end
 
-    it "test_sell_in_decreases_brie" do
+    it "sell in decreases brie" do
       items = [Item.new("Aged Brie", 8, 10)]
       GildedRose.new(items).update_quality
       expect(items[0].sell_in).to eq(7)
     end
 
-    it "test_sell_in_decreases_backstage_pass" do
+    it "sell in decreases backstage pass" do
       items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 8, 10)]
       GildedRose.new(items).update_quality
       expect(items[0].sell_in).to eq(7)
     end
 
     # TODO - convert next 3 tests to 1 parametrized test
-    it "test_sell_in_can_be_negative_generic_item" do
+    it "sell in can be negative generic item" do
       items = [Item.new("generic item", 0, 25)]
       GildedRose.new(items).update_quality
       expect(items[0].sell_in).to eq(-1)
     end
 
-    it "test_sell_in_can_be_negative_brie" do
+    it "sell in can be negative brie" do
       items = [Item.new("Aged Brie", 0, 25)]
       GildedRose.new(items).update_quality
       expect(items[0].sell_in).to eq(-1)
     end
 
-    it "test_sell_in_can_be_negative_backstage_pass" do
+    it "sell in can be negative backstage pass" do
       items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 0, 25)]
       GildedRose.new(items).update_quality
       expect(items[0].sell_in).to eq(-1)
     end
 
-    it "test_generic_item_quality_decreases_before_sell_by" do
+    it "generic item quality decreases before sell by" do
       items = [Item.new("generic item", 5, 10)]
       GildedRose.new(items).update_quality
       expect(items[0].quality).to eq(9)
     end
 
-    it "test_generic_item_quality_decreases_twice_as_fast_after_sell_by" do
+    it "generic item quality decreases twice as fast after sell by" do
       items = [Item.new("generic item", 0, 10)]
       GildedRose.new(items).update_quality
       expect(items[0].quality).to eq(8)
     end
 
-    it "test_generic_item_quality_does_not_go_negative" do
+    it "generic item quality does not go negative" do
       items = [Item.new("generic item", 0, 0)]
       GildedRose.new(items).update_quality
       expect(0).to eq(items[0].quality)
     end
 
-    it "test_brie_quality_increase_before_sell_by" do
+    it "brie quality increase before sell by" do
       items = [Item.new("Aged Brie", 5, 30)]
       GildedRose.new(items).update_quality
       expect(items[0].quality).to eq(31)
     end
 
-    it "test_brie_quality_has_an_upper_limit" do
+    it "brie quality has an upper limit" do
       items = [Item.new("Aged Brie", 5, 50)]
       GildedRose.new(items).update_quality
       expect(items[0].quality).to eq(50)
     end
 
-    it "test_backstage_pass_quality_has_an_upper_limit" do
+    it "backstage pass quality has an upper limit" do
       items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 50)]
       GildedRose.new(items).update_quality
       expect(items[0].quality).to eq(50)
     end
 
-    it "test_brie_quality_increases_twice_as_fast_after_sell_by" do
+    it "brie quality increases twice as fast after sell by" do
       items = [Item.new("Aged Brie", -1, 20)]
       GildedRose.new(items).update_quality
       expect(22).to eq(items[0].quality)
     end
 
-    it "test_brie_quality_has_upper_limit_even_when_really_old" do
+    it "brie quality has upper limit even when really old" do
       items = [Item.new("Aged Brie", -99, 50)]
       GildedRose.new(items).update_quality
       expect(50).to eq(items[0].quality)
     end
 
-    it "test_backstage_pass_quality_increases_each_day_when_concert_date_is_far_off_in_future" do
+    it "backstage pass quality increases each day when concert date is far off in future" do
       items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 30, 23)]
       GildedRose.new(items).update_quality
       expect(24).to eq(items[0].quality)
     end
 
-    it "test_backstage_pass_quality_increases_more_as_concert_date_gets_closer" do
+    it "backstage pass quality increases more as concert date gets closer" do
       items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 10, 40)]
       GildedRose.new(items).update_quality
       expect(42).to eq(items[0].quality)
     end
 
-    it "test_backstage_pass_quality_increases_much_more_when_concert_date_is_close" do
+    it "backstage pass quality increases much more when concert date is close" do
       items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 5, 40)]
       GildedRose.new(items).update_quality
       expect(43).to eq(items[0].quality)
     end
 
-    it "test_backstage_pass_quality_drops_to_zero_when_concert_has_passed" do
+    it "backstage pass quality drops to zero when concert has passed" do
       items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 0, 40)]
       GildedRose.new(items).update_quality
       expect(0).to eq(items[0].quality)
     end
 
-    it "test_backstage_pass_quality_respects_maximum_value_even_when_concert_is_near" do
+    it "backstage pass quality respects maximum value even when concert is near" do
       items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 5, 49)]
       GildedRose.new(items).update_quality
       expect(50).to eq(items[0].quality)
     end
 
-    it "test_shop_contains_multiple_items_and_all_are_updated" do
-      items = [Item.new("Sulfuras, Hand of Ragnaros", 0, 80),
-        Item.new("generic item", 10, 5)]
+    it "shop contains multiple items and all are updated" do
+      items = [
+        Item.new("Sulfuras, Hand of Ragnaros", 0, 80),
+        Item.new("generic item", 10, 5)
+      ]
       GildedRose.new(items).update_quality
 
       expect(items[0].sell_in).to eq(0)
